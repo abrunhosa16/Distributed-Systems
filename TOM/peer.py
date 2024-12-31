0f649c6f16986e6a9b2a41d413a6d508ccc128cf
abrunhosa16 <abrunhosavinicius@gmail.com>
2024-12-31 16:01:19 +0000

    a

--- TOM/peer.py
+++ TOM/peer.py
@@ -132,6 +132,7 @@ def print_message():
             if msg_info[1] != 'ack':
                 if mes_clock not in processed:
                     processed.add(mes_clock)
+                    processed
                     print(mes_clock, msg_info)
 
      
