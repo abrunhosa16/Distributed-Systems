af88ce628cf1d5c7a9147f71e4e5a1545648c02c
abrunhosa16 <abrunhosavinicius@gmail.com>
2025-01-02 16:20:38 +0000

    a

--- TOM/peer.py
+++ TOM/peer.py
@@ -78,7 +78,7 @@ def handle_connection(client: socket.socket, node: PeerNode, client_address):
         ip_peer, word, receiv_clock = received_data
 
         if word == 'shutdown':
-            propagate_shutdown(node)
+            print('shut')
             client.close()
             sys.exit(0)
 
