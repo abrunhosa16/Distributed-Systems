338b813b2e78972838a794ce9957fc1bf896f7dc
abrunhosa16 <abrunhosavinicius@gmail.com>
2025-01-02 16:25:03 +0000

    a

--- TOM/peer.py
+++ TOM/peer.py
@@ -41,6 +41,7 @@ def signal_handler(sig, frame):
                 node.logger.info(f"Sent shutdown signal to {peer}")
         except Exception as e:
             node.logger.warning(f"Failed to send shutdown signal to {peer}: {e}")
+    sys.exit(0)
 
 # Vincular o manipulador ao sinal de interrupção (Ctrl+C)
 signal.signal(signal.SIGINT, signal_handler)
@@ -78,7 +79,6 @@ def handle_connection(client: socket.socket, node: PeerNode, client_address):
 
         if word == 'shutdown':
             print('shut')
-            client.close()
             sys.exit(0)
 
         if word == 'ready':
