71e984445ad8ccd885f0edb9e0e48ae3c1c0ebdf
abrunhosa16 <abrunhosavinicius@gmail.com>
2025-01-02 19:50:39 +0000

    a

--- token ring/inject.py
+++ token ring/inject.py
@@ -2,6 +2,7 @@ import socket
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 12348
+print(HOST)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 client.bind((HOST, PORT))
