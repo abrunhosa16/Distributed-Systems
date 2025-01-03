21a0b548e4ecdaecca2b92e6e637129b2c60a4a5
abrunhosa16 <abrunhosavinicius@gmail.com>
2025-01-03 09:20:50 +0000

    a

--- token ring/inject.py
+++ token ring/inject.py
@@ -8,7 +8,7 @@ print(HOST)
 try:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     client.bind((HOST, PORT))
-    client.connect(('l813', 33333))
+    client.connect(('l813', 44444))
 
     client.send(str('token').encode('UTF-8'))
 
